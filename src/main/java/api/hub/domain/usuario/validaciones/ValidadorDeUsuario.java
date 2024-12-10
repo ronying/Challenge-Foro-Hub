aad7d50da69d3a865b92d306bf08f5e8088a58c5
Rony Castejon <60992883+ronying@users.noreply.github.com>
2024-12-09 22:36:38 -0600

    Initial commit

---
+++ src/main/java/api/hub/domain/usuario/validaciones/ValidadorDeUsuario.java
@@ -0,0 +1,9 @@
+package api.hub.domain.usuario.validaciones;
+
+import api.hub.domain.usuario.RegistroUsuarioDTO;
+import api.hub.domain.usuario.RespuestaUsuarioDTO;
+
+public interface ValidadorDeUsuario {
+
+    public void validate(RegistroUsuarioDTO RegistroUsuarioDTO);
+}
